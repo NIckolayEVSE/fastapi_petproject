0325eecc9a20ababc2769b0806883a563d0e87c2
NIckolayEVSE <biomaster2014@yandex.ru>
2024-08-09 18:12:54 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from datetime import date
-
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 
 from booking.router import router as router_booking
-from schemas import SHotel
+from hotels.router import router as router_hotels
 from users.router import router as router_users
 
 app = FastAPI(
@@ -13,30 +11,7 @@ app = FastAPI(
 
 app.include_router(router_users)
 app.include_router(router_booking)
-
-
-@app.get("/hotel/", response_model=list[SHotel])
-async def get_hostels(
-    # obj: GetHotels,
-    location: str,
-    date_from: date,
-    date_to: date,
-    has_spa: bool = None,
-    stars=Query(None, ge=1, le=5),
-):
-    hotels = [
-        {
-            "address": "ул. Гагарина, 1, Щелково",
-            "name": "Hotel",
-            "stars": 5,
-        }
-    ]
-    return hotels
-
-
-# @app.post("/bookings")
-# async def add_booking(booking: SBooking):
-#     pass
+app.include_router(router_hotels)
 
 
 if __name__ == "__main__":
